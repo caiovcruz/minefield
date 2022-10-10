57659eb92c32f904b3a5e5beb8d6070b65881779
Caio Cruz <covu@gft.com>
2022-10-10 16:14:58 -0300

    Creating class Field with properties and methods, implemented tests.

---
+++ lib/models/field.dart
@@ -0,0 +1,90 @@
+import 'explosion_exception.dart';
+
+class Field {
+  final int row;
+  final int column;
+  final List<Field> neighbors = [];
+
+  bool _isOpen = false;
+  bool _isMarked = false;
+  bool _isMined = false;
+  bool _isExploded = false;
+
+  Field({
+    required this.row,
+    required this.column,
+  });
+
+  void addNeighbor(Field neighbor) {
+    final deltaRow = (row - neighbor.row).abs();
+    final deltaColumn = (column - neighbor.column).abs();
+
+    if (deltaRow == 0 && deltaColumn == 0) {
+      return;
+    }
+
+    if (deltaRow <= 1 && deltaColumn <= 1) {
+      neighbors.add(neighbor);
+    }
+  }
+
+  void open() {
+    if (_isOpen) {
+      return;
+    }
+
+    _isOpen = true;
+
+    if (_isMined) {
+      _isExploded = true;
+      throw ExplosionException();
+    }
+
+    if (safeNeighborhood) {
+      for (var neighbor in neighbors) {
+        neighbor.open();
+      }
+    }
+  }
+
+  void revealMine() {
+    if (_isMined) {
+      _isOpen = true;
+    }
+  }
+
+  void mine() {
+    _isMined = true;
+  }
+
+  void toggleMark() {
+    _isMarked = !_isMarked;
+  }
+
+  void restart() {
+    _isOpen = false;
+    _isMarked = false;
+    _isMined = false;
+    _isExploded = false;
+  }
+
+  bool get isMined => _isMined;
+
+  bool get isExploded => _isExploded;
+
+  bool get isOpen => _isOpen;
+
+  bool get isMarked => _isMarked;
+
+  bool get solved {
+    bool minedAndMarked = isMined && isMarked;
+    bool safeAndOpen = !isMined && isOpen;
+    return minedAndMarked || safeAndOpen;
+  }
+
+  bool get safeNeighborhood =>
+      neighbors.every((neighbor) => !neighbor._isMined);
+
+  int get amountNeighborhoodMines =>
+      neighbors.where((neighbor) => neighbor.isMined).length;
+}
---
+++ test/models/field_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:minefield/models/field.dart';
+
+main() {
+  group('Field', () {
+    test('Open field WITH explosion', () {
+      Field field = Field(row: 0, column: 0);
+      field.mine();
+
+      expect(field.open, throwsException);
+    });
+
+    test('Open field WITHOUT explosion', () {
+      Field field = Field(row: 0, column: 0);
+      field.open();
+
+      expect(field.isOpen, isTrue);
+    });
+
+    test('Add NOT neighbor', () {
+      Field field = Field(row: 0, column: 0);
+      Field fieldNotNeighbor = Field(row: 1, column: 3);
+      field.addNeighbor(fieldNotNeighbor);
+
+      expect(field.neighbors.isEmpty, isTrue);
+    });
+
+    test('Add neighbor', () {
+      Field field = Field(row: 3, column: 3);
+      Field fieldNeighbor1 = Field(row: 3, column: 4);
+      Field fieldNeighbor2 = Field(row: 2, column: 2);
+      Field fieldNeighbor3 = Field(row: 4, column: 4);
+      field.addNeighbor(fieldNeighbor1);
+      field.addNeighbor(fieldNeighbor2);
+      field.addNeighbor(fieldNeighbor3);
+
+      expect(field.neighbors.length, 3);
+    });
+
+    test('Mines in Neighborhood', () {
+      Field field = Field(row: 3, column: 3);
+      Field fieldNeighbor1 = Field(row: 3, column: 4);
+      fieldNeighbor1.mine();
+      Field fieldNeighbor2 = Field(row: 2, column: 2);
+      fieldNeighbor2.mine();
+      Field fieldNeighbor3 = Field(row: 4, column: 4);
+      field.addNeighbor(fieldNeighbor1);
+      field.addNeighbor(fieldNeighbor2);
+      field.addNeighbor(fieldNeighbor3);
+
+      expect(field.amountNeighborhoodMines, 2); 
+    });
+  });
+}
